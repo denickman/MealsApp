8cd4dc6a2cdc641a91deb86b1df380fc7d23d09d
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-23 19:22:37 +0300

    Add _selectCategory to push MealsScreen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:mealapp/data/dummy_data.dart';
+import 'package:mealapp/screens/meals.dart';
 import 'package:mealapp/widgets/category_grid_item.dart';
+import 'package:mealapp/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    //Navigator.push(context, route);
+    /*
+    MaterialPageRoute — это реализация маршрута (Route), которая:
+    Поддерживает анимацию перехода в стиле Material Design (например, слайд слева направо на Android).
+    Оборачивает новый экран (builder) в правильный контекст Material (например, для темизации).
+    Управляет жизненным циклом экрана (инициализация/дестрой).
+    MaterialPageRoute отвечает за то, чтобы переход был «материальный» (с анимацией и правильной темой).
+
+    Альтернативы
+      - Можно было бы использовать PageRouteBuilder, если хочешь кастомную анимацию.
+      - Или вместо MaterialPageRoute использовать CupertinoPageRoute — 
+      и тогда на iOS будет анимация в стиле iOS (свайп назад и т.д.).
+      - Для более сложных навигаций часто используют GoRouter или auto_route.
+    */
+
+
+
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: category.title, meals: filteredMeals),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +48,14 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: [
+          
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList();
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category, 
+              onSelectCategory: () {
+                _selectCategory(context, category);
+            })
         ],
       ),
     );
