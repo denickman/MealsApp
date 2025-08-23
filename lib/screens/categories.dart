387b456f68db2a2cdf9a89033d5fdab3abd8b713
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-23 17:21:29 +0300

    Add InkWell

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Pick your category')),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -9,23 +9,30 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return Container(
-    padding: EdgeInsets.all(16),
-    decoration: BoxDecoration(
-      gradient: LinearGradient(colors: [
-        category.color.withAlpha(130),
-        category.color.withAlpha(200),
-      ],
-      begin: Alignment.topLeft,
-      end: Alignment.bottomRight
-      )
-    ),
-    child: Text(
-      category.title, 
-      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-        color: Theme.of(context).colorScheme.onSurface,
-      )
-      )
+   return InkWell(
+    onTap: () {
+
+    },
+    splashColor: Theme.of(context).primaryColor,
+    borderRadius: BorderRadius.circular(16),
+     child: Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          category.color.withAlpha(130),
+          category.color.withAlpha(200),
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight
+        )
+      ),
+      child: Text(
+        category.title, 
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        )
+        )
+     ),
    );
   }
 }
