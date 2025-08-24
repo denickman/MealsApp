b2a962373b9636a3a242fbc332c7d71e7aa6db31
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-24 12:14:00 +0300

    Add metadata to MealItem

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:mealapp/models/meal.dart';
+import 'package:mealapp/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:mealapp/widgets/meal_item_trait.dart';
 
 class MealItem extends StatelessWidget {
 
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,7 +93,24 @@ class MealItem extends StatelessWidget {
 
                   Row(
                     children: [
+                          MealItemTrait(
+                            icon: Icons.schedule, 
+                            label: "${meal.duration} min"
+                            ),
 
+                            const SizedBox(width: 12),
+
+                            MealItemTrait(
+                              icon: Icons.schedule,
+                              label: complexityText
+                            ),
+
+                            const SizedBox(width: 12),
+
+                            MealItemTrait(
+                              icon: Icons.attach_money,
+                              label: affordabilityText,
+                            )
                     ],
                   )
 
